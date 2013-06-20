ab6fef95449706890834c56f7b86ae2806d9b24f
Julien Pivotto <roidelapluie@inuits.eu>
2013-06-20 21:34:30 +0200

    update transtation

--- subscribeform/models.py
+++ subscribeform/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext as _
 class boothDemand(models.Model):
     first_name = models.CharField(_(u'First name'), max_length=64)
     last_name = models.CharField(_(u'Last name'), max_length=64)
-    birth_date = models.IntegerField(_(u'Birth date'))
+    birth_date = models.DateField(_(u'Birth date'), help_text=_(u'YYYY-MM-DD'))
     mail = models.EmailField(_(u'Email address'))
 
     def __unicode__(self):
